d145c9ce59445e2fb265ff213ff08151ea447c68
Team1093 <log3.official@gmail.com>
2025-01-01 01:35:20 +0900

    seongsu video added

--- src/renderer/src/screens/A5LastInfoScreen.tsx
+++ src/renderer/src/screens/A5LastInfoScreen.tsx
@@ -1,8 +1,8 @@
 'use client'
 import styles from '../styles/A5LastInfoScreen.module.css'
 import {Settings} from '../service/settings/interface'
-import ko_ending from '../assets/videos/ending_guide.mp4'
-import en_ending from '../assets/videos/ending_guide.mp4'
+import underdog_ending from '../assets/videos/ending_guide.mp4'
+import seongsu_ending from '../assets/videos/seongsu_video.mp4'
 
 interface A5LastInfoScreenProps {
   nextScreen: (screenNumber: number) => void
@@ -10,8 +10,8 @@ interface A5LastInfoScreenProps {
 }
 
 const A5LastInfoScreen: React.FC<A5LastInfoScreenProps> = ({ nextScreen, settings }) => {
-  const { lang } = settings
-  const videoSrc = lang === 'ko' ? ko_ending : en_ending;
+  const { location } = settings
+  const videoSrc = location === 'innerview' ? underdog_ending : seongsu_ending;
   return (
     <div className={styles.bg}>
       <video autoPlay className={styles.video} onEnded={() => nextScreen(6)}>
